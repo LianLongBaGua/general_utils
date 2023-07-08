67a80512bbc8d8722c9bedbf65f8feca2259d11d
LianLongBaGua <126940855+LianLongBaGua@users.noreply.github.com>
2023-07-09 07:26:50 +0800

    updated

---
+++ graph_utilities.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from vnpy_ctastrategy.backtesting import BacktestingEngine, OptimizationSetting
+import plotly.graph_objects as go
+
+def graph_result(x, y, result):
+
+    z_dict = {}
+    for param_str, target, statistics in result:
+        param = eval(param_str)
+        z_dict[(param[x], param[y])] = target
+
+    z = []
+    for x_value in x:
+        z_buf = []
+        for y_value in y:
+            z_value = z_dict[(x_value, y_value)]
+            z_buf.append(z_value)
+        z.append(z_buf)   
+
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+    fig.update_layout(
+        title='RESULT', autosize = False,
+        width=600, height=600,
+        scene={
+            'xaxis':{'title': x},
+            'yaxis':{'title': y},
+            'zaxis':{'title': optimization_setting.target_name},
+        },
+        margin={'l':65, 'r':50, 'b':65, 't':90}
+    )
+    fig.show()
--- useful_tools.py
+++ useful_tools.py
@@ -8,10 +8,15 @@ from vnpy.trader.constant import Interval, Exchange
 from sklearn.preprocessing import StandardScaler
 
 
-def load_essentials(symbol: str, start: str, exchange: str):
+def load_essentials(symbol: str, start: str, end: str, exchange: str):
     """Load data from database with end=now and interval=1m"""
     db = Database()
-    df = db.load_bar_df(symbol, Exchange(exchange), Interval.MINUTE, datetime.strptime(start, '%Y-%m-%d'), datetime.now())
+    df = db.load_bar_df(
+        symbol, Exchange(exchange), 
+        Interval.MINUTE, 
+        datetime.strptime(start, '%Y-%m-%d'), 
+        datetime.strptime(end, '%Y-%m-%d')
+        )
 
     trim_df(df)
 
@@ -29,17 +34,18 @@ def prep_label(df, col: str, shift: int, label: str, noise=0.01):
     up = 1
     down = -1
     noise = 0
+    rows with NaN values are dropped
     """
     def compare_values(row, column1: str, column2: str, noise=noise):
         percent_diff = (row[column2] - row[column1]) / row[column1]
 
         if label == 'one_hot':
             if percent_diff > noise:
-                return [1, 0, 0]
+                return np.array([1, 0, 0])
             elif percent_diff < -noise:
-                return [0, 0, 1]
+                return np.array([0, 0, 1])
             else:
-                return [0, 1, 0]
+                return np.array([0, 1, 0])
         elif label == 'binary':
             if percent_diff > noise:
                 return 1
@@ -52,6 +58,25 @@ def prep_label(df, col: str, shift: int, label: str, noise=0.01):
 
     df[f'{shift}_move_label'] = df.apply(compare_values, axis=1, args=(col, col + "_shifted"))
 
+    df.dropna(inplace=True)
+
+
+def create_sequences(df, col, seq_length):
+    """
+    create list of 'col' price sequences of length 'seq_length'
+    TODO: add option to create sequences of multiple columns
+    """
+    xs = []
+    ys = []
+
+    for i in range(df.shape[0]-seq_length-1):
+        x = df[col].iloc[i:(i+seq_length)]
+        xs.append(x)
+        y = df[f'-{seq_length}_move_label'].iloc[i+seq_length]
+        ys.append(y)
+
+    return np.array(xs), np.array(ys)
+
 
 def trim_df(df: pd.DataFrame, keep_symbol=False):
     """Trim everything other than OHLCV with option to keep symbol"""
@@ -61,7 +86,7 @@ def trim_df(df: pd.DataFrame, keep_symbol=False):
         df.drop(['symbol'], axis=1, inplace=True)
 
 
-def standardize(df, cols: list = [], scale_all=False):
+def standardize(df, cols: list = [], scale_all: bool = False):
     """
     Standardize columns
     Use the same standardization for all columns except volume and open_interest
@@ -78,3 +103,18 @@ def standardize(df, cols: list = [], scale_all=False):
     else:
         for col in cols:
             df[col] = scaler.fit_transform(df[col].values.reshape(-1, 1))
+
+
+def divide_means(list1, list2):
+
+    if len(list1) != len(list2):
+        raise ValueError("The lists must have the same length.")
+
+      # Calculate the means of the arrays in each list.
+
+    means1 = [np.mean(array) for array in list1]
+    means2 = [np.mean(array) for array in list2]
+
+      # Divide the means of the arrays in list1 by the means of the arrays in list2.
+
+    return [mean1 / mean2 for mean1, mean2 in zip(means1, means2)]
